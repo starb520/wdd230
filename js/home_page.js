9fdd64ca4958ebbc9a5e8b9440ce7d8812ca11f2
starb520 <97459015+starb520@users.noreply.github.com>
2022-09-19 10:47:22 -0600

    added a dynamic year in js

--- js/home_page.js
+++ js/home_page.js
@@ -1,3 +1,6 @@
 let date = new Date();
 let currentDate = date.toLocaleString();
+
+let year = date.getFullYear();
+document.getElementById('year').textContent = year;
 document.getElementById('currentDate').textContent=currentDate;
