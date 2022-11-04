de3950556a1cfcbbca911fec2506a642bc3c00b9
starb520 <97459015+starb520@users.noreply.github.com>
2022-11-04 09:58:27 -0600

    fixed some javascript and images for discover page

---
+++ chamber/js/discover.js
@@ -0,0 +1,69 @@
+let date = new Date();
+let currentDate = date.toLocaleString();
+let year = date.getFullYear();
+
+const full_date = new Intl.DateTimeFormat('en-US', {dateStyle: 'full'}).format(date);
+
+document.getElementById('date').textContent = full_date;
+document.getElementById('year').textContent = year;
+document.getElementById('current_date').textContent = currentDate;
+
+
+// javascript for days since last visit calculation
+if (!localStorage.getItem('lastvisit')) {
+    localStorage.setItem('lastvisit', Date.now());
+    document.getElementById('diff').textContent = 'This is your 1st visit!';
+} else {
+    setStyles();
+}
+
+function setStyles() {
+    let prevDate = localStorage.getItem('lastvisit');
+    let currDate = new Date();
+
+    let difference = currDate - prevDate;
+        console.log(difference);
+        let daysDifference = Math.floor(difference/1000/60/60/24);
+
+    document.getElementById('diff').textContent = daysDifference;
+
+    localStorage.setItem('lastvisit', Date.now());
+}
+
+
+// javascript for the lazy load of the discovery page pictures
+const pimages = document.querySelectorAll('[data-src]');
+const options = {
+    threshold: 1,
+    rootMargin: "0px 0px 100px 0px"
+}
+
+function preloadImage(img){
+    const source = img.getAttribute('data-src');
+    if (!source) {
+        return;
+    }
+
+    img.src = source;
+}
+
+const io = new IntersectionObserver (
+    (entries, io) => {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                preloadImage(entry.target);
+                io.unobserve(entry.target);
+            }
+        })
+
+    },
+    
+        options
+    
+);
+
+pimages.forEach(image => {
+    io.observe(image);
+})
--- chamber/js/index.js
+++ chamber/js/index.js
@@ -21,61 +21,61 @@ if (date.getDay() == 1 || date.getDay() == 3) {
 }
 
 
-// javascript for days since last visit calculation
-if (!localStorage.getItem('lastvisit')) {
-    localStorage.setItem('lastvisit', Date.now());
-    document.getElementById('diff').textContent = 'This is your 1st visit!';
-} else {
-    setStyles();
-}
+// // javascript for days since last visit calculation
+// if (!localStorage.getItem('lastvisit')) {
+//     localStorage.setItem('lastvisit', Date.now());
+//     document.getElementById('diff').textContent = 'This is your 1st visit!';
+// } else {
+//     setStyles();
+// }
 
-function setStyles() {
-    let prevDate = localStorage.getItem('lastvisit');
-    let currDate = new Date();
+// function setStyles() {
+//     let prevDate = localStorage.getItem('lastvisit');
+//     let currDate = new Date();
 
-    let difference = currDate - prevDate;
-        console.log(difference);
-        let daysDifference = Math.floor(difference/1000/60/60/24);
+//     let difference = currDate - prevDate;
+//         console.log(difference);
+//         let daysDifference = Math.floor(difference/1000/60/60/24);
 
-    document.getElementById('diff').textContent = daysDifference;
+//     document.getElementById('diff').textContent = daysDifference;
 
-    localStorage.setItem('lastvisit', Date.now());
-}
+//     localStorage.setItem('lastvisit', Date.now());
+// }
 
 
-// javascript for the lazy load of the discovery page pictures
-const pimages = document.querySelectorAll('[data-src]');
-const options = {
-    threshold: 1,
-    rootMargin: "0px 0px 100px 0px"
-}
+// // javascript for the lazy load of the discovery page pictures
+// const pimages = document.querySelectorAll('[data-src]');
+// const options = {
+//     threshold: 1,
+//     rootMargin: "0px 0px 100px 0px"
+// }
 
-function preloadImage(img){
-    const source = img.getAttribute('data-src');
-    if (!source) {
-        return;
-    }
+// function preloadImage(img){
+//     const source = img.getAttribute('data-src');
+//     if (!source) {
+//         return;
+//     }
 
-    img.src = source;
-}
+//     img.src = source;
+// }
 
-const io = new IntersectionObserver (
-    (entries, io) => {
-        entries.forEach(entry => {
-            if (!entry.isIntersecting) {
-                return;
-            } else {
-                preloadImage(entry.target);
-                io.unobserve(entry.target);
-            }
-        })
+// const io = new IntersectionObserver (
+//     (entries, io) => {
+//         entries.forEach(entry => {
+//             if (!entry.isIntersecting) {
+//                 return;
+//             } else {
+//                 preloadImage(entry.target);
+//                 io.unobserve(entry.target);
+//             }
+//         })
 
-    },
+//     },
     
-        options
+//         options
     
-);
+// );
 
-pimages.forEach(image => {
-    io.observe(image);
-})
+// pimages.forEach(image => {
+//     io.observe(image);
+// })
