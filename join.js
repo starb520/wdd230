aa4caf79f7b1be31e976bd5819148e1e316d043c
starb520 <97459015+starb520@users.noreply.github.com>
2022-11-05 16:55:32 -0600

    working on javascript errors

--- join.js
+++ join.js
@@ -4,8 +4,9 @@ let year = date.getFullYear();
 
 const full_date = new Intl.DateTimeFormat('en-US', {dateStyle: 'full'}).format(date);
 
-document.getElementById('date').textContent = full_date;
+document.getElementsByClassName('date').textContent = full_date;
 document.getElementById('year').textContent = year;
 document.getElementById('current_date').textContent = currentDate;
 
 document.querySelector('.date').value = full_date;
+// document.querySelector('#date').value = full_date;
