d5e36e36d2495ad0e70a4f4e4bd89c19334e4b72
starb520 <97459015+starb520@users.noreply.github.com>
2022-11-30 21:28:27 -0700

    final project beginning

---
+++ final/js/index.js
@@ -0,0 +1,9 @@
+// let date = new Date();
+// let currentDate = date.toLocaleString();
+// let year = date.getFullYear();
+
+// const full_date = new Intl.DateTimeFormat('en-US', {dateStyle: 'full'}).format(date);
+
+// document.getElementById('date').textContent = full_date;
+// document.getElementById('year').textContent = year;
+// document.getElementById('current_date').textContent = currentDate;
