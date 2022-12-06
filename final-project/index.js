e7d403a06863eb11349a3a0d52bcabe369a82fca
starb520 <97459015+starb520@users.noreply.github.com>
2022-12-06 12:23:42 -0700

    developed header and footer final project

---
+++ final-project/index.js
@@ -0,0 +1,19 @@
+// hamburger button functionality
+function toggleMenu() {
+    document.getElementById("nav-menu").classList.toggle("open");
+    document.getElementById("hamburgerBtn").classList.toggle("open");
+}
+  
+const x = document.getElementById('hamburgerBtn');
+x.onclick = toggleMenu;
+
+// get current date for footer
+let date = new Date();
+let currentDate = date.toLocaleString();
+let year = date.getFullYear();
+
+const full_date = new Intl.DateTimeFormat('en-US', {dateStyle: 'full'}).format(date);
+
+document.getElementById('date').textContent = full_date;
+// document.getElementById('year').textContent = year;
+// document.getElementById('current_date').textContent = currentDate;
