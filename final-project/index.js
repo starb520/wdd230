1c79a668bed8817dd8f4fc66834a609c2b2b1594
starb520 <97459015+starb520@users.noreply.github.com>
2022-12-06 17:40:04 -0700

    changed nav menu to grid for better responsiveness

--- final-project/index.js
+++ final-project/index.js
@@ -1,3 +1,5 @@
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?id=5605242&appid=dcce337c9c67d9fe15707481f83fd5e3";
+
 // hamburger button functionality
 function toggleMenu() {
     document.getElementById("nav-menu").classList.toggle("open");
@@ -9,11 +11,37 @@ x.onclick = toggleMenu;
 
 // get current date for footer
 let date = new Date();
-let currentDate = date.toLocaleString();
-let year = date.getFullYear();
-
 const full_date = new Intl.DateTimeFormat('en-US', {dateStyle: 'full'}).format(date);
-
 document.getElementById('date').textContent = full_date;
-// document.getElementById('year').textContent = year;
-// document.getElementById('current_date').textContent = currentDate;
+
+
+// display the current weather
+const getWeather = async () => {
+    const response = await fetch(apiURL);
+    const jsObject = await response.json();
+    console.log(jsObject);
+    // °F = (K - 273.15)* 1.8000 + 32.00 equation for reference
+    document.querySelector('#temp').textContent = ((jsObject.main.temp - 273.15) * 1.8 + 32).toFixed(0);
+    const iconsrc= `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    const desc = jsObject.weather[0].description;
+    document.querySelector('.weather-img').setAttribute('src', iconsrc);
+    document.querySelector('figcaption').textContent = desc;
+    const windspeed = jsObject.wind.speed;
+    console.log(windspeed);
+    document.querySelector('#windsp').textContent = windspeed; 
+
+    
+const temp = document.querySelector('#temp').textContent;    
+    if (temp < 50 && windspeed > 3) {
+        let chill = Math.round((35.74 + (0.6215 * temp)) - (35.75 * Math.pow(windspeed, 0.16)) + (0.4275 * temp * Math.pow(windspeed,0.16)));
+    
+        const windchill = document.querySelector('#windch');
+    
+        windchill.innerHTML = chill + '&#8457;';
+    }
+    else {
+        const windchill = document.querySelector('#windch');
+        windchill.innerHTML = 'N/A';
+    }
+  };
+getWeather();5605242
